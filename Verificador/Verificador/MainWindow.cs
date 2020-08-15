4c900b1a31ad478c792e6287a336e794b4c71796
luisbaquiax <luis.baquiax1995@gmail.com>
2020-08-15 05:44:51 -0600

    analizador lexico

---
+++ Verificador/Verificador/MainWindow.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Gtk;
+using Verificador.Verificador;
+
+public partial class MainWindow : Gtk.Window
+{
+    public MainWindow() : base(Gtk.WindowType.Toplevel)
+    {
+        Build();
+        Lexema lexema = new Lexema("hola a todos");
+        lexema.mostrarLexemas();
+    }
+
+    protected void OnDeleteEvent(object sender, DeleteEventArgs a)
+    {
+        Application.Quit();
+        a.RetVal = true;
+    }
+
+    protected void OnVerLexemasClicked(object sender, EventArgs e)
+    {
+
+    }
+}
---
+++ Verificador/Verificador/Verificador/Lexema.cs
@@ -0,0 +1,33 @@
+﻿using System;
+namespace Verificador.Verificador
+{
+    public class Lexema
+    {
+        private String oracion;
+       
+
+        public Lexema(String oracion)
+        {
+            this.oracion = oracion;
+        }
+
+        public void mostrarLexemas() {
+
+            String texto = this.oracion;
+            String[] palabras = texto.Split(' ');
+            for (int i = 0; i < palabras.Length; i++) {
+                Console.WriteLine(palabras[i]); 
+            }
+        }
+
+        public Boolean verLetras(String subcadena)
+        {
+            char[] cadenitas = subcadena.ToCharArray;
+            foreach (int i = 0; i < cadenitas.Length; i++) {
+                if (cadenitas[i] == 'a' || (cadenitas[i] == 'A') { 
+                }
+            };
+        }
+
+    }
+}
